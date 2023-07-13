5fdab6e9e6ef21a6ff324e20fee0ee8fd4bfbba3
russellandrady <russellsuper2000@gmail.com>
2023-07-13 21:08:51 +0530

    Tutor_to_Totoring_in_banner.inc.php

--- inc/banner.inc.php
+++ inc/banner.inc.php
@@ -1,3 +1,3 @@
-<h1><a href="index.php">UOK<span> Online Tutor Platform</span></a></h1>
+<h1><a href="index.php">UOK<span> Online Tutoring Platform</span></a></h1>
 <h3><a href="tel:0761670953p123">Hotline:011xxxxxxx</a></h3>
 
