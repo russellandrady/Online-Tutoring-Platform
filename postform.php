3bb89a4bb319ec08637e0625cb2edcd3004788a6
russellandrady <russellsuper2000@gmail.com>
2023-07-19 22:20:56 +0530

    bugfixed

--- postform.php
+++ postform.php
@@ -169,7 +169,7 @@ $list_check = new checkboxlist();
 
 			 ?>
 			<a class="navlink" href="#contact">Contact</a>
-			<a class="navlink" href="about us.html">About</a>
+			<a class="navlink" href="aboutus.php">About</a>
 			<div style="float: right;" >
 				<table>
 					<tr>
